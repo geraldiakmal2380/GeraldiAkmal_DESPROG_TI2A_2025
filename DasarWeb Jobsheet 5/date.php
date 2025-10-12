3f4d41a6b90108037ab3d0b9bc9ac4ede94ce008
geraldiakmal2380 <geraldiakmal612@Gmail.com>
2025-10-12 22:06:28 +0700

    commit di pc

---
+++ DasarWeb Jobsheet 5/date.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";  
+            echo "Today is " . date("1");
+        ?>
+    </body>
+</html>
